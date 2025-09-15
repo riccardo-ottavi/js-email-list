2eba04615401a42bd27fcd4d5b00f8f45d4edd2a
RiccardoOttavi <ottavi.ps@libero.it>
2025-09-15 16:22:29 +0200

    finalizza out in pagina

--- script.js
+++ script.js
@@ -4,33 +4,22 @@ const emailsOut = document.getElementById("emails-out");
 //seleziono end point di boolean
 const endPoint = "https://flynn.boolean.careers/exercises/api/random/mail";
 
-
+//chiamo la funzione che genera le 10 mail assegnando il tutto a una const
 const myMailList = generateTenMails();
-writeArrayInPage();
-
-
-function writeArrayInPage(){
-    for (let i = 0; i < 10; i++){
-        let newLi = document.createElement("li");
-        newLi.innerText = i;
-        emailsOut.appendChild(newLi);
-    }
-}
-
-
-
-
-
 
 function generateTenMails() {
 let result = [];
-//richiedo le mail e intanto le stampo in console
+//richiedo le mail e intanto le scrivo in pagina (10 volte)
 for (let i = 0; i < 10; i++){
     axios.get(endPoint)
         .then(risposta => {
             //in caso di successo
             let randomMail = risposta.data.response;
             result.push(randomMail);
+            //manda le mail in pagina
+            let newLi = document.createElement("li");
+            newLi.innerText = randomMail;
+            emailsOut.appendChild(newLi);
         })
         .catch(error =>{
             //in caso di errore
