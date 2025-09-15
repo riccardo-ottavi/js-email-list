876658a6597e3d9267eba91f8a602104892c3271
RiccardoOttavi <ottavi.ps@libero.it>
2025-09-15 15:16:55 +0200

    ottieni la risposta alla chiamata nella console

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("prova");
+//seleziono l'out in pagina
+const emailsOut = document.getElementById("emails-out");
+
+//seleziono end point di boolean
+const endPoint = "https://flynn.boolean.careers/exercises/api/random/mail";
+
+//richiedo le mail e intanto le stampo in console
+axios.get(endPoint)
+    .then(risposta => {
+        //in caso di successo
+        const result = risposta.data.response;
+        console.log(result);
+    })
+    .catch(error =>{
+        //in caso di errore
+        console.error(error);
+    })
